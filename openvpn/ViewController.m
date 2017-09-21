5c8541e02c2e72dbf1d502af64e3559a2717eb90
suanti <drumarovsergey@gmail.com>
2017-09-21 17:43:05 +0500

    first

--- openvpn/ViewController.m
+++ openvpn/ViewController.m
@@ -16,7 +16,35 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    NEVPNManager *manager = [NEVPNManager sharedManager];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(vpnConnectionStatusChanged) name:NEVPNStatusDidChangeNotification object:nil];
+    [manager loadFromPreferencesWithCompletionHandler:^(NSError *error) {
+        if(error) {
+            NSLog(@"Load error: %@", error);
+        }}];
+    NEVPNProtocolIPSec *p = [[NEVPNProtocolIPSec alloc] init];
+    p.username = @“[My username]”;
+    p.passwordReference = [KeyChainAccess loadDataForServiceNamed:@"VIT"];
+    p.serverAddress = @“[My Server Address]“;
+    p.authenticationMethod = NEVPNIKEAuthenticationMethodCertificate;
+    p.localIdentifier = @“[My Local identifier]”;
+    p.remoteIdentifier = @[My Remote identifier]”;
+    p.useExtendedAuthentication = NO;
+    p.identityData = [My VPN certification private key];
+    p.disconnectOnSleep = NO;
+    [manager setProtocol:p];
+    [manager setOnDemandEnabled:NO];
+    [manager setLocalizedDescription:@"VIT VPN"];
+    NSArray *array = [NSArray new];
+    [manager setOnDemandRules: array];
+    NSLog(@"Connection desciption: %@", manager.localizedDescription);
+    NSLog(@"VPN status:  %i", manager.connection.status);
+    [manager saveToPreferencesWithCompletionHandler:^(NSError *error) {
+        if(error) {
+            NSLog(@"Save error: %@", error);
+        }
+    }];
 }
 
 
